e8d977011b949a33b700776a4ad1a3e73b7ea185
NyOnitiana <rafelantsoanyonitiana@gmail.com>
2026-08-16 12:19:10 +0300

    correction

--- Arduino/functions.cpp
+++ Arduino/functions.cpp
@@ -4,11 +4,11 @@
 void detectionPresence()
 {
   float d;
-  d = 21.5;
+  d = 19.60;
   Ultrason U;
   U.setPinEcho(8);
   U.setPinTrig(7);
-  if(U.calculDistance()<d || U.calculDistance()>23)
+  if(U.calculDistance()<d || U.calculDistance()>20)
   {
     Serial.println("P");
   }
